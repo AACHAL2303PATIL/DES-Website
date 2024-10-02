8d7481e82b5328d3efa881375cbfdf3f117c5eaf
AACHAL2303PATIL <aachalpatil2307@gmail.com>
2024-10-02 20:09:36 +0530

    feat:companies path removed

--- pages/companies.vue
+++ pages/companies.vue
@@ -3,22 +3,19 @@
     <AppSection :group="companyGroup" />
   </main>
 </template>
-
 <script setup lang="ts">
-import { companies } from '@/constants/companies'; // Adjust the path as necessary
-
 // Alphabetically sort companies
-const sortedCompanies = companies.sort((a, b) => a.name.localeCompare(b.name));
+const sortedCompanies = companies.sort((a, b) => a.name.localeCompare(b.name))
 
-const companyGroup = { name: 'Edmonton Companies', items: sortedCompanies };
+const companyGroup = { name: 'Edmonton Companies', items: sortedCompanies }
 
-const title = 'Edmonton Companies';
-const description = 'A list of Edmonton companies to help the Dev Edmonton community.';
+const title = 'Edmonton Companies'
+const description = 'A list of Edmonton companies to help the Dev Edmonton community.'
 
 useServerSeoMeta({
   title,
   description,
-});
+})
 </script>
 
 <style scoped>
